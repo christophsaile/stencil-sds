e311b2810f575a4aa528e57afec81b26930c8b88
Christoph Saile <christoph.saile@virtual-identity.com>
2024-02-12 16:25:21 +0100

    feat: init project

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,5 +1,7 @@
 import { Component, Prop, h } from '@stencil/core';
 import { format } from '../../utils/utils';
+import '@solid-design-system/components/dist/package/components/button/button'
+import type SdButton from '@solid-design-system/components/dist/package/components/button/button'
 
 @Component({
   tag: 'my-component',
@@ -22,11 +24,20 @@ export class MyComponent {
    */
   @Prop() last: string;
 
+  private refSdButton: SdButton;
+
   private getText(): string {
     return format(this.first, this.middle, this.last);
   }
 
+  componentDidLoad() {
+    this.refSdButton.classList.add('red');
+    this.refSdButton.variant = 'primary';
+    this.refSdButton.variant = 'test';
+  }
+
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div>Hello, World! I'm {this.getText()}
+    <sd-button variant='test' ref={(el) => this.refSdButton = el}>Hello world</sd-button></div>;
   }
 }
